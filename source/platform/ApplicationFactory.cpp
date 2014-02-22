ec80993f3aca7030dfd316bea6b697af64eb63a4
Jorge Israel Peña <jorgepblank@gmail.com>
2014-02-22 01:35:44 -0800

    all use SDL

--- source/platform/ApplicationFactory.cpp
+++ source/platform/ApplicationFactory.cpp
@@ -3,14 +3,6 @@
 #include "Application.h"
 #include "SDLApplication.h"
 
-// #ifdef WIN32
-// #include "WindowsApplication.h"
-// #elif APPLE
-// #include "MacApplication.h"
-// #else
-// #include "LinuxApplication.h"
-// #endif
-
 namespace Coasters {
 namespace Platform {
 
@@ -19,18 +11,7 @@ ApplicationFactory::ApplicationFactory() {}
 ApplicationFactory::~ApplicationFactory() {}
 
 std::unique_ptr<Application> ApplicationFactory::CreateApplication() {
-  std::unique_ptr<Application> app;
-
-  app.reset(new SDLApplication);
-
-// #ifdef WIN32
-//   app.reset(new WindowsApplication);
-// #elif APPLE
-//   app.reset(new MacApplication);
-// #elif UNIX
-//   app.reset(new LinuxApplication);
-// #endif
-
+  std::unique_ptr<Application> app(new SDLApplication);
   return app;
 }
 
